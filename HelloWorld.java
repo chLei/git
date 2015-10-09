a175c88806dd0671531aed7fe265c626e0203a9d
chlei <993716335@qq.com>
2015-10-09 22:40:49 +0800

    modify HelloWorld.java and add a new file .gitignore

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,10 @@
 public Class HelloWorld{
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
+        System.out.println("Hello");
+        String[] strArr = new String[12];
+        for (String str : strArr) {
+            str = "HelloWorld";
+            System.out.println(str);
+        }
     }
 }
